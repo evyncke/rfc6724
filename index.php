8f195e3971d97eef99b012525038f5607cd3a210
Eric Vyncke <eric@vyncke.org>
2024-01-30 16:18:17 +0100

    Layout

--- index.php
+++ index.php
@@ -59,41 +59,32 @@ Using code from https://github.com/whitequark/ipaddr.js
 <!-- End Matomo Code -->
 </head>
 <body onload="init('<?=$policy?>');">
-<h1>RFC 6724 on-line</h1>
+<div class="container">
+
+<div class="row">
+<p class="text-end">Link:
+<a id="pageURL" href="<?=$_SERVER['PHP_SELF']?>" title="share the address set + policy choice"><i class="bi bi-link-45deg"></i></a>
+<i class="bi bi-copy link-primary" onclick="navigator.clipboard.writeText(document.getElementById('pageURL').href)" title="Copy the link"></i></p>
+</div><!--row-->
+
+<h1 class="display-1">RFC 6724 on-line</h1>
 
 <p><small>You need to input two valid source IPv4/IPv6 addresses and at least one valid destination IPv4/IPv6 address.
 The address scope, label, and preference are automatically computed and displayed. As soon as
 two source addresses and at least one destination address are validated, then the best
 pair of source and destination is evaluated against
-<select id="policySelect" onchange="loadPolicy(this.value);">
+<select id="policySelect" onchange="loadPolicy(this.value);" class="form-select-sm">
 <option value="rfc6724">RFC 6724</option>
 <option value="rfc3484">RFC 3484</option>
 <option value="linux-5.15">linux-5.15 (2024)</option>
 <option value="rfc6724-bis">draft-ietf-6man-rfc6724-update-06</option>
-</select>.</small></p>
+</select>. Click on <i class="bi bi-chevron-expand text-primary"></i> to expand the rule evaluation.</small></p>
 
 <div class="row">
-<div class="col-xs-12 col-md-6">
-<h2>Source addresses</h2>
-Address#1: <input type="text" id="src1" onkeyup="addrChanged(this);" length="32" placeholder="2001:db8::1" value="<?=$src1?>">
-<span id="span_src1"></span>
-<br/>
-Address#2: <input type="text" id="src2" onkeyup="addrChanged(this);" length="32" placeholder="Any IPv4/IPv6 address" value="<?=$src2?>">
-<span id="span_src2"></span>
-</div><!-- col -->
-
-<div class="col-xs-12 col-md-6">
-<h2>Destination address(es)</h2>
-Address#1: <input type="text" id="dst1" onkeyup="addrChanged(this);" length="32" placeholder="2001:db8:2::1" value="<?=$dst1?>">
-<span id="span_dst1"></span>
-<br/>
-Address#2: <input type="text" id="dst2" onkeyup="addrChanged(this);" length="32" placeholder="Can be empty" value="<?=$dst2?>">
-<span id="span_dst2"></span>
-</div><!-- col -->
-</div><!--row-->
-<div class="row">
-<p>Or select an example set of addresses from RFC 6724 sections 10.1 and 10.2:
+<h2>Addresses</h2>
+<p><small>Please enter the IPv4/IPv6 addresses below or select an example set of addresses from RFC 6724 sections 10.1 and 10.2:
 <select id="setId" onchange="addressesChanged(this);" name="addressesSet" class="form-select w-auto">
+<option disabled>Preload address examples from RFC 6724</option>
 <option value="1">1 - Source: prefer appropriate scope</option>
 <option value="2">2 - Source: prefer appropriate scope</option>
 <option value="3">3 - Source: prefer same address</option>
@@ -107,20 +98,42 @@ Address#2: <input type="text" id="dst2" onkeyup="addrChanged(this);" length="32"
 <option value="15">15 - Destination: longest matching prefix</option>
 <option value="16">16 - Destination: prefer matching label</option>
 <option value="17">17 - Destination: prefer higher precedence</option>
+<option disabled>-- About draft-ietf-6man-rfc6724-update --</option>
 <option value="98">98 - draft-ietf-6man-rfc6724-update: ULA-ULA preferred over IPv4-IPv4</option>
 <option value="99">99 - draft-ietf-6man-rfc6724-update: IPv4-IPv4 preferred over ULA-GUA</option>
 </select>
 </p>
-</div><!--row-->
+</div><!-- row -->
+
 <div class="row">
-<p>Link to this page (to share the address set + policy choice):<a id="pageURL" href="<?=$_SERVER['PHP_SELF']?>"><i class="bi bi-link-45deg"></i></a>
-<i class="bi bi-copy link-primary" onclick="navigator.clipboard.writeText(document.getElementById('pageURL').href)"></i>.</p>
+<div class="col-xs-12 col-md-6">
+<h3>Source</h3>
+Address#1: <input type="text" id="src1" onkeyup="addrChanged(this);" length="32" placeholder="2001:db8::1" value="<?=$src1?>">
+<span id="span_src1"></span>
+<br/>
+Address#2: <input type="text" id="src2" onkeyup="addrChanged(this);" length="32" placeholder="Any IPv4/IPv6 address" value="<?=$src2?>">
+<span id="span_src2"></span>
+</div><!-- col -->
+
+<div class="col-xs-12 col-md-6">
+<h3>Destination</h3>
+Address#1: <input type="text" id="dst1" onkeyup="addrChanged(this);" length="32" placeholder="2001:db8:2::1" value="<?=$dst1?>">
+<span id="span_dst1"></span>
+<br/>
+Address#2: <input type="text" id="dst2" onkeyup="addrChanged(this);" length="32" placeholder="Can be empty" value="<?=$dst2?>">
+<span id="span_dst2"></span>
+</div><!-- col -->
 </div><!--row-->
+
 <hr>
 <div class="row">
 <div id="policy" class="col-md-6 w-auto"></div><!-- policy-->
 </div><!-- row -->
+
+<hr>
+
 <span id="sas"></span>
 <span id="das"></span>
+</div><!-- container-->
 </body>
 </html>
